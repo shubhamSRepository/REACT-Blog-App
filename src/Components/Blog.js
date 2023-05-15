de681fc473f0e7707287c439b9bd4fe5292ec3f5
shubhamSRepository <soni.shubham927@gmail.com>
2023-05-15 13:44:48 +0530

    passing an object to useState

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,8 +3,14 @@ import { useState } from "react";
 //Blogging App using Hooks
 export default function Blog() {
 
-    const [title, setTitle] = useState();
-    const [content, setContent] = useState();
+    // const [title, setTitle] = useState();
+    // const [content, setContent] = useState();
+
+
+    /*we will pass an object to useState so that we don't have to make two 'useStates' each for title and content */
+    const [formData, setFormData] = useState({ title: "", content: "" });
+
+
     const [blogs, setBlogs] = useState([]);
 
 
@@ -13,9 +19,12 @@ export default function Blog() {
     function handleSubmit(e) {
         e.preventDefault();
 
-        setBlogs([{ title, content }, ...blogs]);
-        setTitle("");
-        setContent("");
+        /*we can setBlogs in both the ways */
+        // setBlogs([formData, ...blogs]);
+        setBlogs([{ title:formData.title, content: formData.content }, ...blogs]);
+
+
+        setFormData({ title: "", content: "" });
     }
 
 
@@ -35,8 +44,8 @@ export default function Blog() {
                     <Row label="Title">
                         <input className="input"
                             placeholder="Enter the Title of the Blog here.."
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
+                            value={formData.title}
+                            onChange={(e) => setFormData({title:e.target.value, content:formData.content})}
                         />
                     </Row >
 
@@ -44,8 +53,8 @@ export default function Blog() {
                     <Row label="Content">
                         <textarea className="input content"
                             placeholder="Content of the Blog goes here.."
-                            value={content}
-                            onChange={(e) => setContent(e.target.value)}
+                            value={formData.content}
+                            onChange={(e) => setFormData({title:formData.title, content:e.target.value})}
                         />
                     </Row >
 
